92cd5f41c24165cae0d0a440894217d6c5a9a4c2
Smeghammer <smeghammer@live.com>
2025-08-01 13:54:20 +0100

    removed local deleted files following re-arrangement of hierarchy

--- backend/test.py
+++
@@ -1,7 +0,0 @@
-import time
-counter=0
-while True or counter > 1500:
-    print(counter)
-    counter +=1
-    time.sleep(1)
-    
--- src/routes/dwbrowser/[id]/stores.js
+++
@@ -1,6 +0,0 @@
-// see https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Svelte_stores
-
-import { writable } from "svelte/store";
-
-// THIS: https://svelte.dev/repl/a551a40e90374fd2861255c957123af2?version=4.2.0
-export const breadcrumb = writable([]);
