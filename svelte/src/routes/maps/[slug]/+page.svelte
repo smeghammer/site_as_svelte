38575fc3c31aee1f0d2e6af43d06a7d48c85aa1a
Smeghammer <smeghammer@live.com>
2025-08-06 15:58:38 +0100

    removes inline secondary nav

--- svelte/src/routes/maps/[slug]/+page.svelte
+++ svelte/src/routes/maps/[slug]/+page.svelte
@@ -2,29 +2,23 @@
     // see https://rodneylab.com/sveltekit-json-import/
     import MapDetail from "$lib/components/MapDetail.svelte";
     import SecondaryNavInline from "$lib/components/SecondaryNavInline.svelte";
-    const displayType:string = "maps";
+    // const displayType:string = "maps";
 
-    // import { page } from '$app/stores'
     let detail = {"title":"Not found","description":"",allImages:[],"download":null}
 
+    /** this includes data from the layout server load function: */
     export let data;
-    // console.log("DATA: ",data)
-    // export let alldata
-    // $: detail = data.data[0]
+    
+    /** THIS data comes from the page server load function: */
     $: detail = data.currentwad.data[0];
 
 // https://www.makeuseof.com/sveltekit-routing-guide/
 </script>
 
 <div class="pure-u-1 pure-u-md-5-6 pure-u-lg-3-4">
-
-    <!-- TODO: Implement 'self' determination -->
     <!-- <SecondaryNavInline {data}></SecondaryNavInline> -->
-    <SecondaryNavInline { data }></SecondaryNavInline>
 
-    <!-- {console.log(detail)} -->
     <!-- this also needs to inherit data from the root layout load function -->
     <MapDetail {detail}/>
-
 </div>
 
--- svelte/src/routes/snippets/[slug]/+page.svelte
+++ svelte/src/routes/snippets/[slug]/+page.svelte
@@ -3,35 +3,22 @@
     // import maps from "$lib/data/maps.json";
     import MapDetail from "$lib/components/MapDetail.svelte";
     import SecondaryNavInline from "$lib/components/SecondaryNavInline.svelte";
-    
-    const displayType:string = "maps";
+    // const displayType:string = "maps";
 
-    // import { page } from '$app/stores'
     let detail = {"title":"Not found","description":"",allImages:[]}
-    let dl_link;
-    // TODO: rework data to key on slug - that way I don't need to loop.
-    // for(let a=0;a<maps.length;a++){
-    //     if(maps[a].slug === $page.params.slug){
-    //         detail = maps[a];
-    //     }
-    // }
-// https://www.makeuseof.com/sveltekit-routing-guide/
 
-export let data; // from layout load() function
-// console.log("DATA: ",data.currentwad)
-// console.log("DATA: ",data.allmaps)
-// $: detail = data.data[0]
-$: detail = data.currentwad.data[0];
+    /** this includes data from the layout server load function: */
+    export let data;
+
+    /** THIS data comes from the page server load function: */
+    $: detail = data.currentwad.data[0];
+    
+    // https://www.makeuseof.com/sveltekit-routing-guide/
 </script>
 
 <div class="pure-u-1 pure-u-md-5-6 pure-u-lg-3-4">
-
-    <!-- TODO: Implement 'self' determination -->
-    <!-- <SecondaryNavInline {data}></SecondaryNavInline> -->
-    <SecondaryNavInline { data }></SecondaryNavInline>
-    
+    <!-- <SecondaryNavInline { data }></SecondaryNavInline> -->
     
     <MapDetail {detail}/>
- 
 </div>
 
