718d02416e2f2b67c95a9acfe93c3564ea2fb0be
Smeghammer <smeghammer@live.com>
2025-08-11 15:34:40 +0100

    removes debug log statement

--- svelte/src/routes/dwbrowser/[id]/+page.svelte
+++ svelte/src/routes/dwbrowser/[id]/+page.svelte
@@ -5,8 +5,6 @@
     export let data;
     $: data = data; //because we will pass additional args on browsing
 
-console.log("PAGE.SVELTE",data)
-
     /** 
      * see https://stackoverflow.com/questions/66698656/access-variable-in-a-component-updated-by-a-different-component-in-svelte 
      * to allow sharing vars between sibling components
@@ -19,7 +17,6 @@ console.log("PAGE.SVELTE",data)
 </script>
 
 <div class="pure-u-1 pure-u-md-3-4 pure-u-lg-5-6">
-    <!-- <h2>ID Games (slug)</h2> -->
 
     <!-- the path component needs to render links with incremental depth attribute values, so going BACK will reset -->
     <IDGamesPath { data } />
@@ -37,6 +34,5 @@ console.log("PAGE.SVELTE",data)
     -->
 
     <IdGamesListContents {data}  />
-
 </div>
 
